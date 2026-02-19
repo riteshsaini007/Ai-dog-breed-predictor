d8b55d651173c62e038f3fcfdbfc5c1919592a68
Ritesh Saini <165307052+riteshsaini007@users.noreply.github.com>
2026-02-19 19:35:32 +0530

    Update download_dataset.py

--- download_dataset.py
+++ download_dataset.py
@@ -2,9 +2,6 @@ from supabase import create_client
 import requests
 import os
 
-# ==============================
-# 🔐 Supabase Credentials
-# ==============================
 
 SUPABASE_URL = "https://cnlanqegjktriwmumlnb.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNubGFucWVnamt0cml3bXVtbG5iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzMjk2ODcsImV4cCI6MjA4NjkwNTY4N30.oX8ssiw9uIB97IISshg8TRIbGy3iSA4Iar5h_6vBVsM"
@@ -13,16 +10,10 @@ supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 BUCKET_NAME = "Dogs_Images"
 
-# ==============================
-# 📂 Fetch all breeds from table
-# ==============================
 
 response = supabase.table("Dogs").select("Breed_Name, folder_name").execute()
 breeds = response.data
 
-# ==============================
-# 📥 Download images breed-wise
-# ==============================
 
 for breed in breeds:
     folder = breed["folder_name"]
@@ -47,4 +38,5 @@ for breed in breeds:
         with open(f"dataset/{folder}/{file_name}", "wb") as f:
             f.write(img_data)
 
-print("\n✅ Dataset Download Complete!")
+print("\n Dataset Download Complete!")
+
